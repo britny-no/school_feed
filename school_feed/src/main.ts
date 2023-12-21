2954f773bfbdbf82710e52e02fb3677bdbd5f42d
britny <njw990@gmail.com>
2023-12-21 19:44:39 +0900

    for dumping table..

--- school_feed/src/main.ts
+++ school_feed/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(configService.get<string>('PORT', '4404'));
 }
 bootstrap();
